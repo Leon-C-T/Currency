ef56019a59f0f4d36094d6fd919ae9dfa72a8801
Leon-C-T <leont1280@gmail.com>
2020-04-07 15:21:22 +0000

    change to docker compose version in dockinst.sh

--- script/docker_install.sh
+++ script/docker_install.sh
@@ -14,7 +14,7 @@ sudo apt install docker-ce docker-ce-cli containerd.io -y
 
 sudo systemctl status docker
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
 
