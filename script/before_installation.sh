0c68ef26bbe96826e4fe7c158eebf6d54d0b39c5
Leon-C-T <leont1280@gmail.com>
2020-04-07 14:46:19 +0000

    jenkinsfile

--- script/before_installation.sh
+++ script/before_installation.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sudo apt update -y
+sudo apt-get update -y
 
 sudo apt install python3 -y
 
 sudo apt install python3-pip -y
+
--- script/installation.sh
+++ script/installation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source venv/bin/activate
+#source venv/bin/activate
 
 python3 -m pip install flask
 
