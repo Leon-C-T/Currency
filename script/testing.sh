811dc327e4a38186495fea574e46088e8b9b8e4b
Leon-C-T <leont1280@gmail.com>
2020-04-14 00:32:42 +0000

    change to script

--- script/testing.sh
+++ script/testing.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 source ~/.bashrc
 pip3 show coverage
-python3 -m coverage run -m --source=. pytest ./test/testing.py  
-python3 -m coverage report 
+python3 -m coverage run -m pytest ./test/testing.py 
+#python3 -m coverage run -m --source=. pytest ./test/testing.py ./Service_1/app.py 
+python3 -m coverage report
 # python3 -m coverage run -m pytest /var/lib/jenkins/workspace/sfia1/test/testing.py
 # python3 -m coverage report
